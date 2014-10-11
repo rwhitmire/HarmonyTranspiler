df91944a43c6354bc243ef4b1dc5b1eb271624c7
Ryan Whitmire <ryanw51@gmail.com>
2014-10-11 07:53:10 -0400

    First commit

---
+++ HarmonyTranspiler.Tests/TranspilerFacts.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+
+namespace HarmonyTranspiler.Tests
+{
+    public class TranspilerFacts
+    {
+        [Fact]
+        public void TranspileShouldGetResult()
+        {
+            var transpiler = new Transpiler();
+            var result = transpiler.Transpile("var foo = 23;");
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TranspileShouldGetResultWithString()
+        {
+            var transpiler = new Transpiler();
+            var result = transpiler.Transpile("var foo = 'bar';");
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TranspileShouldGetResultWithDoubleQuotedString()
+        {
+            var transpiler = new Transpiler();
+            var result = transpiler.Transpile("var foo = \"bar\";");
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TranspileShouldHandleImport()
+        {
+            var transpiler = new Transpiler();
+            var result = transpiler.Transpile(@"import foo from 'foo';");
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TranspileShouldHandleExport()
+        {
+            var transpiler = new Transpiler();
+            var result = transpiler.Transpile(@"export default {foo: 'bar'};");
+
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ HarmonyTranspiler/Transpiler.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Reflection;
+using MsieJavaScriptEngine;
+
+namespace HarmonyTranspiler
+{
+    public class Transpiler
+    {
+        public string Transpile(string script)
+        {
+            var engine = new MsieJsEngine();
+
+            engine.Execute("module = {}; exports = {};");
+
+            var transpiler = GetTranspiler();
+
+            engine.Execute(transpiler);
+
+            script = EscapeSingleQuotes(script);
+
+            engine.Execute(string.Format("var compiler = new module.exports.Compiler('{0}');", script));//    "var compiler = new module.exports.Compiler('" + script + "');");
+
+            return engine.Evaluate<string>("compiler.toAMD();");
+
+        }
+
+        private static string GetTranspiler()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            const string resourceName = "HarmonyTranspiler.Resources.es6-module-transpiler.js";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var result = reader.ReadToEnd();
+                return result;
+            }
+        }
+
+        private static string EscapeSingleQuotes(string script)
+        {
+            return script.Replace("'", "\\'");
+        }
+    }
+}
